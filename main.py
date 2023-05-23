f279811af4ec80d92c1227e59d6278962ad8512b
Caik Franco <caikfra@alu.ufc.br>
2023-05-22 22:00:59 -0300

    arquivo main.py testado

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-"""Main functions"""
+def is_palindrome(string:str)-> bool:
+    string = string.lower()
+    indesejados = "! ? , . ; : - _ "
+    desejados = [c for c in string if c not in indesejados]
+    palavras = "".join(desejados)
+    inverso = ""
+    for letra  in range(len(palavras) -1 , -1 , -1):
+        inverso += palavras[letra]
+    if inverso == palavras:
+        return True
+    else: 
+        return False
 
 
-def is_palindrome(string: str) -> bool:
-    """Check if string is palindrome."""
-    return False
+
+is_palindrome("Anotaram a data da maratona!")
+    
